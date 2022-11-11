40cddfbcd7a71a437d8cff5d3e72d42206da59e2
skoyev <skoyev@gmail.com>
2022-11-10 22:58:13 -0500

    Dev

--- wp-content/themes/zerif-lite-child/functions.php
+++ wp-content/themes/zerif-lite-child/functions.php
@@ -13,5 +13,5 @@ function my_theme_enqueue_styles() {
 
     //wp_enqueue_script( 'wpa-main-js', get_stylesheet_directory_uri() . '/js/main.js', array(), time(), true );
 
-    wp_enqueue_script( 'wpa-main-js', get_stylesheet_directory_uri() . '/js/main.js', array(), 1, true );
+    wp_enqueue_script( 'wpa-main-js', get_stylesheet_directory_uri() . '/js/main.js', array(), 2, true );
 }
