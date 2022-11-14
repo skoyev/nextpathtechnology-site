e62f7a74d9fc271319101306f5caa75ead489830
skoyev <skoyev@gmail.com>
2022-11-13 21:49:31 -0500

    Dev

--- wp-content/themes/zerif-lite-child/header.php
+++ wp-content/themes/zerif-lite-child/header.php
@@ -14,6 +14,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 <?php wp_head(); ?>
 
