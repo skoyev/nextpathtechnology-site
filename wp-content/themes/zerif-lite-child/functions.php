d7be373c91636e7c0fba837724f36e83eed1d238
skoyev <skoyev@gmail.com>
2022-11-13 21:36:53 -0500

    Dev

--- wp-content/themes/zerif-lite-child/functions.php
+++ wp-content/themes/zerif-lite-child/functions.php
@@ -5,9 +5,18 @@ add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     $my_js_ver  = date("ymd-Gis", filemtime( plugin_dir_path( __FILE__ ) . `js/main.js` ));
 
-    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+    //wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 
-    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri().'/style.css', array(), '0.1.0', 'all' );
+    //wp_enqueue_style( 'child-style', get_stylesheet_directory_uri().'/style.css', array(), '0.1.0', 'all' );
+
+    $parent_style = 'parent-style';
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );    
     
     //wp_enqueue_script( 'wpa-main-js', get_theme_file_uri( 'js/main.js' ), [], null, true );
 
