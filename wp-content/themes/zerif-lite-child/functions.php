f67b827fb5dc549b9c55a106d9f4d65a2dad7646
skoyev <skoyev@gmail.com>
2022-11-22 09:14:39 -0500

    Dev

--- wp-content/themes/zerif-lite-child/functions.php
+++ wp-content/themes/zerif-lite-child/functions.php
@@ -5,7 +5,7 @@ function my_theme_enqueue_styles() {
 
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 
-    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri().'/style.css', array(), '0.1.1' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri().'/style.css', array(), '0.1.2' );
 
     //$parent_style = 'parent-style';
 
