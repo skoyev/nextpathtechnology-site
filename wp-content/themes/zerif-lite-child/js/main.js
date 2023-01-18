0cbee54cd6983901e22ad218f3336138bd56d9e0
skoyev <skoyev@gmail.com>
2023-01-18 09:32:47 -0500

    Development

--- wp-content/themes/zerif-lite-child/js/main.js
+++ wp-content/themes/zerif-lite-child/js/main.js
@@ -161,15 +161,15 @@ jQuery( document ).ready(function($) {
             .first().addClass( "slideshow" ); */
 
         if(slideshow) {
-            let slider = '<div>2</div>';
+            let slider = '<div><div class="container" style="display: none;"><h1 class="intro-text">WE DELIVER INNOVATIVE SOFTWARE SOLUTIONS TO GROW YOUR BUSINESS</h1><h4 style="color:#FFF;width: 50%; margin-left: auto; margin-right: auto;">We digitiazlize your business and leverage latest cloud technologies, make your business secured and scalable.</h4></div></div>';
             // slider 2
             slideshow.append(slider);            
 
-            slider = '<div>3</div>';
+            slider = '<div><div class="container" style="display: none;"><h1 class="intro-text">WE DELIVER INNOVATIVE SOFTWARE SOLUTIONS TO GROW YOUR BUSINESS</h1><h4 style="color:#FFF;width: 50%; margin-left: auto; margin-right: auto;">We digitiazlize your business and leverage latest cloud technologies, make your business secured and scalable.</h4></div></div>';
             // slider 3
             slideshow.append(slider);            
 
-            slider = '<div>4</div>';
+            slider = '<div><div class="container" style="display: none;"><h1 class="intro-text">WE DELIVER INNOVATIVE SOFTWARE SOLUTIONS TO GROW YOUR BUSINESS</h1><h4 style="color:#FFF;width: 50%; margin-left: auto; margin-right: auto;">We digitiazlize your business and leverage latest cloud technologies, make your business secured and scalable.</h4></div></div>';
             // slider 4
             slideshow.append(slider);      
             
