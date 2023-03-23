c3f37cd84d9c26d8c696e2331390a55cb10d2192
skoyev <skoyev@gmail.com>
2023-03-22 22:46:43 -0400

    Development

--- wp-content/themes/zerif-lite-child/js/main.js
+++ wp-content/themes/zerif-lite-child/js/main.js
@@ -23,8 +23,8 @@ jQuery( document ).ready(function($) {
         var img1   = baseurl + "/2020/01/software_healthcare-solutions.jpg";
 
         let service1 = '<div class="col-md-3" style="background-image: url(' + img1+ '); height: 360px; background-repeat:no-repeat;">1</div>';
-        let service2 = '<div class="col-md-3" style="background-image: url(' + img1+ '); height: 360px; background-repeat:no-repeat;">2</div>';
-        let service3 = '<div class="col-md-3" style="background-image: url(' + img1+ '); height: 360px; background-repeat:no-repeat;">3</div>';
+        let service2 = '<div class="col-md-3 col-md-offset-1" style="background-image: url(' + img1+ '); height: 360px; background-repeat:no-repeat;">2</div>';
+        let service3 = '<div class="col-md-3 col-md-offset-1" style="background-image: url(' + img1+ '); height: 360px; background-repeat:no-repeat;">3</div>';
 
         let servicesCnt = `<div class="row">${service1}${service2}${service3}</div>`;
         services.html(servicesCnt);
