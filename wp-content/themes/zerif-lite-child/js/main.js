7e78a0407f9e16a33d2f0cde7800f771aaa6b817
skoyev <skoyev@gmail.com>
2023-04-16 11:35:49 -0400

    Development

--- wp-content/themes/zerif-lite-child/js/main.js
+++ wp-content/themes/zerif-lite-child/js/main.js
@@ -156,8 +156,8 @@ jQuery( document ).ready(function($) {
 			$('#email-msg').html('<span style="font-size:18px;">There was an error while sending your request.</span>');
 		   }
            $('#email-msg').delay(5000).fadeOut('slow');
-           $('#email-msg').html('<span style="font-size:18px;"></span>');
-           $('#email-msg').fadeIn('slow');
+           $('#email-msg').delay(5000).html('<span style="font-size:18px;"></span>');
+           $('#email-msg').delay(5000).fadeIn('slow');
 		});
 
   		event.preventDefault();
